c374bbc2ccafe69992fa481a5f525786b87932eb
RakibMernDev <abdullahalrakib30@gmail.com>
2025-08-29 01:05:03 +0600

    feat: enhance signup process with validation and add JWT token generation utility

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,18 +1,46 @@
-export const signup = (req, res) => {
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
 
   try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long." });
+    }
 
-    
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "Email already in use." });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+if (newUser) {
+
+
+  
+}
+
+else {
+  res.status(400).json({ message: "Invalid user data." });
+}
 
 
 
-  } catch (error) {
-
-
-
-  }
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
+
 const app = express();
 
 const PORT = process.env.PORT || 5002;
