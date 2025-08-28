f9c1271775e6c84b15b8434c3d7a3749a9388ed0
RakibMernDev <abdullahalrakib30@gmail.com>
2025-08-29 01:54:34 +0600

    refactor: streamline signup process by removing unused JWT import and enhancing error handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -28,19 +28,23 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
 
-if (newUser) {
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
 
-
-  
-}
-
-else {
-  res.status(400).json({ message: "Invalid user data." });
-}
-
-
-
-  } catch (error) {}
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data." });
+    }
+  } catch (error) {
+    console.log("Error in signup:", error.message);
+    res.status(500).json({ message: "Server error." });
+  }
 };
 
 export const login = (req, res) => {
