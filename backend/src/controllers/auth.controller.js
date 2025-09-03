64eeb69bcad7542810a723e6c930bb99d6af3b52
RakibMernDev <abdullahalrakib30@gmail.com>
2025-09-04 04:28:49 +0600

    feat: implement async login functionality with error handling and token generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -51,8 +51,33 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login route");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid username or password." });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signup:", error.message);
+    res.status(500).json({ message: "Server error." });
+  }
 };
 
 export const logout = (req, res) => {
