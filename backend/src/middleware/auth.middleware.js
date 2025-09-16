a088890af1e91d48b517c9c9e03f1a17f602ddf2
RakibMernDev <abdullahalrakib30@gmail.com>
2025-09-16 20:03:57 +0600

    added protected route middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,12 +5,31 @@ export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
 
-if (!token) {
-      return res.status(401).json({ message: "Unauthorized, no token provided" });
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized, no token provided" });
     }
 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized, invalid token" });
+    }
 
-    
-  } catch (error) {}
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "Unauthorized, user not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+
+console.log("Error in auth middleware", error.message);
+    res.status(500).json({ message: "Server error." });
+
+  }
 };
