12d5d8fc2604ad96d578d490276bc4565809cb8e
RakibMernDev <abdullahalrakib30@gmail.com>
2025-10-31 00:13:54 +0600

    feat: add routing with react-router-dom and create placeholder pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,23 @@
-
-
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
-     <div>
-      
-      <Navbar/>
+      <div>
+        <Navbar />
 
-
-
-     </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />S{" "}
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
