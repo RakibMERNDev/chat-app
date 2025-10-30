99c87436bc2fc3b352e2c82d996232e17639d224
RakibMernDev <abdullahalrakib30@gmail.com>
2025-10-30 10:26:49 +0600

    refactor: reorganize message controller functions and update routes

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,74 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 
-export const sendMessage = async (req, res) => {
-  try {
-    const { message } = req.body;
-    const { id: receiverId } = req.params;
-
-    const senderId = req.user._id;
-
-    let conversation = await Conversation.findOne({
-      participants: {
-        $all: [senderId, receiverId],
-      },
-    });
-
-    if (!conversation) {
-      conversation = await Conversation.create({
-        participants: [senderId, receiverId],
-      });
-    }
-
-    const newMessage = new Message({
-      senderId,
-      receiverId,
-      message,
-    });
-
-    if (newMessage) {
-      conversation.messages.push(newMessage._id);
-    }
-
-    //!! Socket.io integration can be done here
-
-    // await conversation.save();
-    // await newMessage.save();
-
-    await Promise.all([conversation.save(), newMessage.save()]);
-
-    res.status(200).json({ message: "Message sent successfully.", newMessage });
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server error." });
-
-    console.log("Error in sendMessage controller", error.message);
-  }
-};
-
-export const getMessages = async (req, res) => {
-  try {
-    const { id: userToChatId } = req.params;
-
-    const userId = req.user._id;
-
-    const conversation = await Conversation.findOne({
-      participants: { $all: [userId, userToChatId] },
-    }).populate("messages"); // Populate messages to get full message details
-
-    if (!conversation) {
-      return res.status(200).json({ messages: [] });
-    }
-
-    const messages = conversation.messages;
-
-    res.status(200).json(messages);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server error." });
-
-    console.log("Error in getMessages controller", error.message);
-  }
-};
-
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
@@ -82,5 +14,64 @@ export const getUsersForSidebar = async (req, res) => {
     console.log("Error in getUsersForSidebar controller", error.message);
 
     res.status(500).json({ message: "Server error." });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+
+    const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        {
+          senderId: myId,
+          receiverId: userToChatId,
+        },
+        {
+          senderId: userToChatId,
+          receiverId: myId,
+        },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages Controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+
+    const { id: receiverId } = req.params;
+
+    const senderId = req.user._id;
+
+    let imageUrl;
+
+    if (image) {
+      // Upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    //! TODO: realtime functionality with websockets
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage Controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
