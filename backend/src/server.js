7b0b0282097e1ea3beb5062e8315738990408724
RakibMernDev <abdullahalrakib30@gmail.com>
2025-11-01 19:09:50 +0600

    feat: add CORS support and lucide-react for loading indicators; update axios base URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,19 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 5002;
 
