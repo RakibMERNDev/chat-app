274cd34d602b9376dd48ed4830c481fc25bf6cb1
RakibMernDev <abdullahalrakib30@gmail.com>
2025-11-01 14:02:15 +0600

    added authstore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,24 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
+  isSigningUp: false,
+  isLoginIng: false,
+  isUpdatingProfile: false,
 
   isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({ authUser: res.data.user });
+    } catch (error) {
+      set({ authUser: null });
+      console.log("Error in checkAuth", error.message);
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
 }));
