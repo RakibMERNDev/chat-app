49ef987d9f540280ed5d114aca672d01cfb0f296
RakibMernDev <abdullahalrakib30@gmail.com>
2025-11-01 13:31:28 +0600

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
@@ -13,7 +18,7 @@ function App() {
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/profile" element={<ProfilePage />} />S{" "}
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </div>
     </>
