bed49944410bd1e38370dca9850cb7579a886a5e
RakibMernDev <abdullahalrakib30@gmail.com>
2025-11-21 20:44:53 +0600

    feat: implement Sidebar, NoChatSelected, and ChatContainer components for enhanced chat functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,7 @@
-
-
+import { useChatStore } from "../store/useChatStore";
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
-
-import { useChatStore } from "../store/useChatStore";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
@@ -14,7 +11,7 @@ const HomePage = () => {
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-            {/* <Sidebar /> */}
+            <Sidebar />
 
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
           </div>
