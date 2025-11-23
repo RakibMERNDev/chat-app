5ee95abf618e9f51497687734c124be7b0fa926e
RakibMernDev <abdullahalrakib30@gmail.com>
2025-11-23 22:49:33 +0600

    feat: add MessageSkeleton component for improved loading state in ChatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import MessageInput from "./MessageInput";
 import ChatHeader from "./ChatHeader";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessageLoading, selectedUser } =
@@ -11,8 +12,15 @@ const ChatContainer = () => {
     getMessages(selectedUser._id);
   }, [selectedUser._id, getMessages]);
 
-  if (isMessageLoading) return <div>Loading...</div>;
-
+  if (isMessageLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
