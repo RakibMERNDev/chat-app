ac66167832a6fa85df7c60fcbd7781c230a4c167
RakibMernDev <abdullahalrakib30@gmail.com>
2025-11-23 22:39:26 +0600

    feat: implement ChatContainer and NoChatSelected components; enhance message loading and user interaction experience

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import MessageInput from "./MessageInput";
+import ChatHeader from "./ChatHeader";
 
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessageLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessageLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>Messages.....</p>
+
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
