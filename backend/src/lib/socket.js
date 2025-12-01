e1b9590c46960c9657ff85c3e3e03cb2473fa425
RakibMernDev <abdullahalrakib30@gmail.com>
2025-12-02 00:45:30 +0600

    added cors on io

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -10,12 +10,16 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-woad-eight-98.vercel.app/",
+    ],
+    credentials: true,
   },
 });
 
 // Function to get socket ID of a user
-export function getReceiverSocketId(userId)  {
+export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
