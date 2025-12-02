79d612e01eae5bbceb96118db145e0be90738ef8
RakibMernDev <abdullahalrakib30@gmail.com>
2025-12-02 21:16:52 +0600

    added cors middleware implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,6 +37,20 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
